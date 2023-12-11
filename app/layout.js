0b6f76728a399bd2476b6f33a0a675b23248755b
mufurkand <mfurkand115@gmail.com>
2023-12-11 09:35:55 +0300

    Add a barebones navbar

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
 // TODO: Discuss if we want to use a different font
 const inter = Inter({ subsets: ["latin"] });
@@ -13,7 +14,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="flex flex-col">
+          <Navbar />
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
---
+++ components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="w-auto h-14 bg-background text-text flex justify-around items-center">
+      <p>Navbar</p>
+      <p>Navbar</p>
+      <p>Navbar</p>
+    </div>
+  );
+};
+
+export default Navbar;
