0c67d6b08a3e4057a1ccd4ef0b94d74a8eedb83b
mufurkand <mfurkand115@gmail.com>
2023-12-12 12:01:39 +0300

    Add and configure Tailwind Prettier plugin

--- app/page.js
+++ app/page.js
@@ -2,6 +2,6 @@ export default function Home() {
   return (
     // sm:* values are for screens larger than 640px
     // if the screen is smaller than 640px, they will not be executed
-    <div className="bg-background sm:w-5/6 w-screen sm:rounded-tl-md"></div>
+    <div className="w-screen bg-background sm:w-5/6 sm:rounded-tl-md"></div>
   );
 }
