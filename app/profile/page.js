272de6343dc1aab7c81733629f6f341e0d016320
mufurkand <mfurkand115@gmail.com>
2023-12-23 20:48:00 +0300

    Add profile edit route

--- app/profile/page.js
+++ app/profile/page.js
@@ -1,5 +1,3 @@
-import React from "react";
-
 const page = () => {
   return (
     <div className="text-center text-2xl font-extrabold text-accentRed">
--- components/utils/Input.jsx
+++ components/utils/Input.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const Input = ({ placeholder, type, name }) => {
   return (
     <input
